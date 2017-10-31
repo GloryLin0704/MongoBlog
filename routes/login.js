dcb8a05ec7ad8ffc53a5a625e9ec297b9a2d5b81
GloryLin0704 <1019022357@qq.com>
2017-11-01 01:23:58 +0800

    小修改

--- routes/login.js
+++ routes/login.js
@@ -2,26 +2,26 @@ var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
 var auth = require('../tools/auth');
-var secret = 'TheBlogSecret'; 
+var secret = 'TheBlogSecret';
 
 var { userModel } = require('../tools/db');
 
 router.get('/', (req, res) => {
     res.json({
         code: 200,
-        data: 'Welcome'    
+        data: 'Welcome'
     });
 })
 
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
     var username = req.body.username;
     var password = crypto.createHash('sha256', secret)
-                    .update(req.body.password).digest('hex');
+        .update(req.body.password).digest('hex');
 
     userModel.findOne({
         username: username
     }).then(user => {
-        if (user && password === user.password){
+        if (user && password === user.password) {
             var token = auth.get({
                 username: username
             });
@@ -30,11 +30,11 @@ router.post('/', function(req, res, next){
                 code: 200,
                 data: token,
                 msg: '登陆成功'
-            }); 
+            });
         } else {
             res.json({
                 code: 400,
-                msg: '用户名或密码错误！'    
+                msg: '用户名或密码错误！'
             });
         };
     });
